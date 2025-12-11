f2ab722e656f7c3f8f900dfc48907da2d0c920c9
DOEN HIDETAKA <hidedon29@gmail.com>
2025-12-11 23:06:22 +0900

    Update image_service.dart

--- lib/image_service.dart
+++ lib/image_service.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-// ★★★ 取得したご自身の API キーに置き換えてください ★★★
-const String PIXABAY_API_KEY = '53654683-dd3eb7dbedf814decc127055b';
-// ★★★ 取得したご自身の API キーに置き換えてください ★★★
+const String PIXABAY_API_KEY = 'Enter Your API Key';
 
 class ImageService {
   // Pixabay APIを使って単語に関連する画像を検索し、URLを返す
@@ -20,10 +18,8 @@ class ImageService {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        // 成功した場合
         final data = json.decode(response.body);
 
-        // ヒットした画像があるか確認
         if (data['hits'] != null && data['hits'].length > 0) {
           // 最初の画像のWebformatURL（中サイズ）を返す
           return data['hits'][0]['webformatURL'];
